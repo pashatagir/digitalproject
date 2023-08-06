f36addc25ce4a87f2be1229137de35770556ecba
pashatagir <108672605+pashatagir@users.noreply.github.com>
2023-08-06 13:46:23 +0000

    Deploying to gh-pages from @ pashatagir/digitalproject@034b06e6606fd1a238176b43aefcd209010de8ee 🚀

--- js/main.131767434a5eca283cd3.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdigitalproject=self.webpackChunkdigitalproject||[]).push([[179],{707:function(){console.log("this is an example")}},function(e){e(e.s=707)}]);
---
+++ js/main.5ed922196cd14ca9d727.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdigitalproject=self.webpackChunkdigitalproject||[]).push([[179],{191:function(){console.log("this is an example")}},function(e){e(e.s=191)}]);
