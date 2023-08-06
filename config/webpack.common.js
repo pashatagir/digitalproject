c766a4a63ec5cd6aabc8a3108a87add535f8b265
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-08-06 21:57:42 +0300

    solving problem with loading images

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,7 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WebpackBar = require('webpackbar');
 const paths = require('./paths');
 
 module.exports = {
@@ -13,36 +14,27 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env']],
-          },
-        },
+        test: /\.js$/,
+        use: ['babel-loader'],
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
       },
       {
-        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
         type: 'asset/inline',
       },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new WebpackBar(),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: paths.public,
-          to: 'assets',
-          globOptions: {
-            ignore: ['*.DS_Store'],
-          },
-          noErrorOnMissing: true,
+          from: paths.src + '/images',
+          to: paths.dist + '/images',
         },
       ],
     }),
